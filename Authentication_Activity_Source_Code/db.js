fb4213989533b0cd800e7d68de0096b0b3ac4f7a
JesusDGP <blizzard339@arizona.edu>
2023-12-06 11:39:17 -0700

    Removed unesesary folder

--- Authentication_Activity_Source_Code/db.js
+++
@@ -1,7 +0,0 @@
-// to use mongoDB
-const mongoose = require("mongoose");
-mongoose.set('strictQuery', true); // or false if you want to suppress this warning
-mongoose.connect("mongodb://127.0.0.1/authen", { useNewUrlParser: true, useUnifiedTopology:true });
-
-
-module.exports = mongoose;
