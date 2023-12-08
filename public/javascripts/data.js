e71a79d08287557183861e7c0ea1eb72bcb12969
JesusDGP <blizzard339@arizona.edu>
2023-12-08 16:13:58 -0700

    Updated graphs

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,5 +1,6 @@
 const dataForBarGraph = [80, 98, 60, 95, 120, 100];
 const dataForLineGraph = {
-  labels: ["2022-01-01", "2022-01-02", "2022-01-03", "2022-01-04", "2022-01-05"],
-  data: [Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100],
+    labels: ["2022-01-01", "2022-01-02", "2022-01-03", "2022-01-04", "2022-01-05"],
+  dataLine1: [80, 70, 60, 70, 90],
+  dataLine2: [95, 96, 97, 96, 99]
 };
