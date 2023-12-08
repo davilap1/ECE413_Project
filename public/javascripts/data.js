d08201b942bbed02b50846bd34c0a80822560a83
JesusDGP <blizzard339@arizona.edu>
2023-12-08 15:31:14 -0700

    Graphs takes data from data.js

---
+++ public/javascripts/data.js
@@ -0,0 +1,5 @@
+const dataForBarGraph = [80, 98, 60, 95, 120, 100];
+const dataForLineGraph = {
+  labels: ["2022-01-01", "2022-01-02", "2022-01-03", "2022-01-04", "2022-01-05"],
+  data: [Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100],
+};
